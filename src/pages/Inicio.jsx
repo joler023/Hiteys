b5915ecfb2ea8a603eeec2ba40307a8f8bfa7dcd
Joel <joelochoa.s023@gmail.com>
2023-02-25 09:57:03 -0500

    estructura v1 inicio

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,10 +1,80 @@
-import React from 'react'
+import React from "react";
 
 const Inicio = () => {
   return (
     <div>
+      <div>
+        <h2>Titulo</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quas
+          omnis dolorum consectetur dolore harum velit.
+        </p>
+        <button>Hola</button>
+      </div>
+      <div>
+        <h3>We bring your vision to reality</h3>
+        <h3>Vision to Reality</h3>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos nam id
+          temporibus ad praesentium, veritatis.
+        </p>
+      </div>
+      <div>
+        <h2>Nuestros servicios</h2>
+        <div>
+          <img src="" alt="" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos nam id
+            temporibus ad praesentium, veritatis.
+          </p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos nam id
+            temporibus ad praesentium, veritatis.
+          </p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos nam id
+            temporibus ad praesentium, veritatis.
+          </p>
+        </div>
+        <button>Ver todos nuestros servicios</button>
+      </div>
+      <div>
+        <h5>Nuestro stack de tecnologias</h5>
+        <h3>Modern Technologies To Drive You Forward</h3>
+        <p>We build on both proven and emerging technologies to guide our clients toward the best solution for their individual challenges.</p>
+        <div>
+          <img src="" alt="" />
+          <h4>React.JS</h4>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h4>Railway</h4>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h4>MySQL</h4>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h4>Bootstrap</h4>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h4>Node.JS</h4>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h4>Express.JS</h4>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Inicio
+export default Inicio;
